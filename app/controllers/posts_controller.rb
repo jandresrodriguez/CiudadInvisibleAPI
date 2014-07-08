0a3fab5c771da56709adc6dae9eb3ba5bb73d6d7
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-07-07 21:18:14 -0300

    Removing security token

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   protect_from_forgery with: :null_session   
+  skip_before_filter :verify_authenticity_token, :only => [:update]
 
   # GET /posts
   # GET /posts.json

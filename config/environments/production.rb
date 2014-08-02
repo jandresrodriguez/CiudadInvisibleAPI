75283556550e18b1530ba2ae230a56ffd3d41213
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-08-02 11:19:58 -0300

    Trying amazonS3 as images provider

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,16 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # config/environments/production.rb
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['ciudadinvisible'],
+      :access_key_id => ENV['AKIAJG7TMYWMOQZBBXXQ'],
+      :secret_access_key => ENV['Kpd6OathLsnX7G4WRVTpP/Ab7cizgZ3qNFbx1/G4']
+    }
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.

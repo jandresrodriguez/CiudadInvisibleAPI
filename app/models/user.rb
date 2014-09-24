9970f54acdecb974740de9b9771d6179113972e4
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-09-23 22:56:04 -0300

    Followers structure added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,25 @@
 class User < ActiveRecord::Base
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
 	
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  	validates :username, presence: true
-  	validates :first_name, presence: true
-  	validates :last_name, presence: true
-  	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+	validates :username, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
 
-  	def file_url
-      # Concatela la url del host mas la de la imagen
-      ActionController::Base.asset_host + avatar.url
-    end
+	def file_url
+    # Concatela la url del host mas la de la imagen
+    ActionController::Base.asset_host + avatar.url
+  end
 
 end

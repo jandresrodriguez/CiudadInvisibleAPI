abad56d8eabacfaf84a0389d4e3435804a4fcb5d
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-10-17 23:50:21 -0200

    Categories get fixed, some seeds about category added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    render json: @categories.to_json(only: :name), status: :ok
   end
 
   # GET /categories/1
@@ -15,6 +16,7 @@ class CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
+    render json: @category, status: :ok
   end
 
   # GET /categories/1/edit

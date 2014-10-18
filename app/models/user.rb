8be3f4d8cf4b5abd09021be1582015e330ffca0e
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-10-18 13:09:12 -0200

    Missing image for users added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   has_many :favorites
   has_many :favorites_posts, through: :favorites, source: :post
 	
-	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://pixrbog.com//imgs/defaultProfile.jpg"
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	validates :username, presence: true

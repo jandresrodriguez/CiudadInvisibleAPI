d925ada9165f396b2848715470966fb355f345a3
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2015-04-06 22:29:30 -0300

    Fixing push notifications

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -18,33 +18,26 @@ class Notifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_notification(notification)
-    user = User.find_by_id(notification.receiver_id)
+    user = User.find_by_id(notification.receiver.id)
     uri = URI.parse("https://api.parse.com/1/push")
     http = Net::HTTP.new(uri.host, uri.port)
-
     request = Net::HTTP::Post.new(uri.request_uri)
     request["X-Parse-Application-Id"] = "JV9KTqeAA1skH0ZiUE8PSzl7PwmnKptuumpj9pqZ"
     request["X-Parse-REST-API-Key"] = "8zWBkSn9F40y22oN8273qHMeSSeSgLqTXRIqfzFb"
     request["Content-Type"] = "application/json"
-    body = "{
-        'where': {
-          'deviceType': 'ios',
-          'deviceToken': '#{user.try(:device_token)}'
-        },
-        'data': {
-          'alert': '#{notification.try(:title)}',
-          'sound': 'default',
-          'payload': '#{notification.try(:message)}'
-        }
-      }"
+    require 'byebug'; byebug
+    body = { where: { 
+              deviceType: "ios", 
+              deviceToken: user.try(:device_token) 
+            }, 
+            data: { 
+              alert: notification.try(:title), 
+              sound: "default", 
+              payload: notification.try(:message) 
+            }
+           }.to_json
     http.use_ssl = true
     response = http.request(request,body)
-    puts "------- DEBUGGING -------"
-    puts "SE MANDOOOOO"
-    puts "#{response}"
-    puts "#{notification.to_json}"
-    puts "#{user.to_json}"
-    puts "-------------------------"
     if user.email
       mail( :to => user.email, :subject => notification.title )  
     end
